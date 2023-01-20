8bf99b6a6271e9cffebb318890a1e9d3f1ed7f87
MrSallivan <nik.sallivan@gmail.com>
2023-01-20 14:32:37 +1000

    22

---
+++ massive.js
@@ -0,0 +1,10 @@
+var cars = ['ford', 'mazda', 'kia', 'bmv']
+var any = [23, 'asd', { a: 2 }]
+
+console.log(cars);
+console.log(cars[1]);
+
+cars.push('yaguar')
+any.pop()
+console.log(cars);
+console.log(any);
